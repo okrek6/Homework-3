5f3e85488bc71be45a2f926ed4e0e0489c7ca37d
Brendan Krekeler <okrek6@gmail.com>
2018-07-15 15:15:39 -0700

    First Commit

---
+++ Homework 3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Homework 3
+//
+//  Created by Brendan Krekeler on 7/15/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
